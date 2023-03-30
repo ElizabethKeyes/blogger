05a21797cbe0f424b1177387e2aed83ce9e3426f
Elizabeth Keyes <liz4momentum@gmail.com>
2023-03-30 14:59:30 -0600

    Small changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,11 +11,11 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto">
-        <li>
+        <!-- <li>
           <router-link :to="{ name: 'About' }" class="btn text-dark selectable text-uppercase">
             About
           </router-link>
-        </li>
+        </li> -->
       </ul>
       <!-- LOGIN COMPONENT HERE -->
       <Login />
